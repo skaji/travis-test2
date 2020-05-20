4b1840e4bf18c33faade6cdc34c3b067ddfcca3f
Shoichi Kaji <skaji@cpan.org>
2020-05-21 08:56:55 +0900

    wip

---
+++ test.pl
@@ -0,0 +1,26 @@
+#!/usr/bin/env perl
+use strict;
+use warnings;
+use 5.10.1;
+use IO::Socket::IP;
+
+my $server = IO::Socket::IP->new(
+    LocalHost => '::1',
+    LocalPort => 8334,
+    Listen => 5,
+    Proto => 'tcp',
+    ReuseAddr => 1,
+) or die "Failed to create a TCP server: $!";
+
+say "Successfully created a TCP server on ::1:8334";
+
+my $client = IO::Socket::IP->new(
+    PeerHost => '::1',
+    PeerPort => 8334,
+);
+
+if ($client) {
+    say "Successfully connected the server";
+} else {
+    say "Failed to connect the server: $!";
+}
