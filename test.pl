a7100c1d5d0605d7f51de05c07b7fcb5bb59e7d5
Shoichi Kaji <skaji@cpan.org>
2020-05-21 20:28:56 +0900

    wip

--- test.pl
+++ test.pl
@@ -1,26 +1,43 @@
 #!/usr/bin/env perl
 use strict;
 use warnings;
-use 5.10.1;
+use feature 'say';
 use IO::Socket::IP;
 
-my $server = IO::Socket::IP->new(
-    Listen => 5,
-    Proto => 'tcp',
-) or die "Failed to create a TCP server, $!";
-
-my $host = $server->sockhost;
-my $port = $server->sockport;
-
-say "Successfully created a TCP server on $host:$port";
-
-my $client = IO::Socket::IP->new(
-    PeerHost => $host,
-    PeerPort => $port,
+my %case = (
+    "set LocalAddr => :: for IO::Socket::IP" => { LocalAddr => '::' },
+    "do not set LocalAddr for IO::Socket::IP" => {},
 );
 
-if ($client) {
-    say "Successfully connected the server via $host:$port";
-} else {
-    say "Failed to connect the server via $host:$port, $!";
+for my $case (sort keys %case) {
+    say "---> Case: $case";
+    my $server = IO::Socket::IP->new(
+        %{$case{$case}},
+        LocalPort => 8445,
+        Listen => 5,
+        Proto => 'tcp',
+    ) or die "Failed to create a TCP server, $!";
+    if (!$server) {
+        say "Failed to create a TCP server, $!";
+        next;
+    }
+
+    my $host = $server->sockhost;
+    my $port = $server->sockport;
+    say "Successfully created a TCP server on $host:$port";
+
+    $host = '127.0.0.1' if $host eq '0.0.0.0';
+    $host = '::1'       if $host eq '::';
+    my $client = IO::Socket::IP->new(
+        PeerHost => $host,
+        PeerPort => $port,
+    );
+
+    if ($client) {
+        say "Successfully connected the server via $host:$port";
+        close $client;
+    } else {
+        say "Failed to connect the server via $host:$port, $!";
+    }
+    close $server;
 }
